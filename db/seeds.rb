e06ec354b3213fa9a749dc7740f51070a336eb1b
mrudenko <mrudenko@vk.team>
2025-03-01 18:16:26 +0300

    4 «Модели и работа с ними. Организация сбора и хранения экспертных оценок в базе данных приложения »

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Image.delete_all
+# Image.reset_pk_sequence
+# Image.create([
+#                {name: 'Мурзик', file: '0.jpg', theme_id: 1},
+#                {name: 'Барсик', file: '1.jpg', theme_id: 1},
+#                {name: 'Рыжик', file: '2.jpg', theme_id: 1},
+#                {name: 'Васька', file: '3.jpg', theme_id: 1},
+#                {name: 'Снежок', file: '4.jpg', theme_id: 1},
+#                {name: 'Тигра', file: '5.jpg', theme_id: 1},
+#                {name: 'Черныш', file: '6.jpg', theme_id: 1},
+#                {name: 'Пушистик', file: '7.jpg', theme_id: 1},
+#                {name: 'Лучик', file: '8.jpg', theme_id: 1},
+#                {name: 'Гав', file: '9.jpg', theme_id: 1}
+#              ])
+
+Theme.delete_all
+Theme.reset_pk_sequence
+Theme.create([
+   {name: "-----"},      # 1 Нет темы
+   {name: "Какое из произведений художника О.Ренуара наилучшим образом характеризует его творчество?"},      # 2
+   # {name: "Какое из произведений художника П.Пикассо наилучшим образом характеризует его творчество?"},      # 3
+   # {name: "Какое из произведений художника А.Матисса наилучшим образом характеризует его творчество?"},      # 4
+ ])
+
+User.delete_all
+User.reset_pk_sequence
+User.create([
+  {name: "Example User", email: "example@railstutorial.org", password: "222222", password_confirmation: "222222"},
+])
